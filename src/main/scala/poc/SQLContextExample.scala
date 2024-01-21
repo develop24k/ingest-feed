f6616e6c427de76972f3a43e4af7e542b0bc266d
develop24k <develop24k@gmail.com>
2024-01-21 12:21:40 +0530

    Initial commit

---
+++ src/main/scala/poc/SQLContextExample.scala
@@ -0,0 +1,34 @@
+package com.develop24k
+
+import org.apache.spark.sql.{SQLContext, SparkSession}
+import com.typesafe.config._
+
+object SQLContextExample extends App {
+
+  val config: Config = ConfigFactory.load()
+
+  val spark = SparkSession.builder()
+    .master("local[1]")
+    .appName(config.getString("appname"))
+    .getOrCreate();
+
+  spark.sparkContext.setLogLevel("ERROR")
+
+
+  val sqlContext:SQLContext = spark.sqlContext
+
+  //read csv with options
+  val df = sqlContext.read.options(Map("inferSchema"->"true","delimiter"->",","header"->"true"))
+    .csv(config.getString("feed_path")).cache()
+
+//  df.repartition(4)
+//    .write
+//    .parquet("c:\\temp\\")
+  df.show()
+  df.printSchema()
+
+  df.createOrReplaceTempView("TAB")
+  sqlContext.sql("select * from TAB")
+    .show(false)
+
+}
